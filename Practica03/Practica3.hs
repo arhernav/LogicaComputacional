e1fee7783a5283121c828c245f82faa27142098d
Armando Hernández Navarro <arhernav@ciencias.unam.mx>
2022-03-29 10:39:41 -0600

    Añadida la practica 3 al repositorio

---
+++ Practica03/Practica3.hs
@@ -0,0 +1,72 @@
+{--
+--
+--
+--}
+module Practica3 where
+
+import Practica2
+
+
+
+{----- Formas Normales -----}
+
+-- 1. fnn. Función que devuelve la Forma Normal Negativa de una 
+--         proposición.
+fnn :: Prop -> Prop
+fnn p = error "Sin implementar."
+
+-- 2. fnc. Función que devuelve la Forma Normal Conjuntiva de una 
+--         proposición.
+fnc :: Prop -> Prop
+fnc p = error "Sin implementar."
+
+
+
+{----- Algoritmo DPLL -----}
+
+-- Definiciones de algunos conceptos.
+type Literal = Prop
+type Clausula = [Literal]
+type Formula = [Clausula]
+type Modelo = [Literal]
+type Solucion = (Modelo, Formula)
+
+
+-- 3. unit. Función que aplica la regla unitaria.
+unit :: Solucion -> Solucion
+unit (m, f) = error "Sin implementar."
+
+-- 4. elim. Función que aplica la regla de eliminación. 
+elim :: Solucion -> Solucion
+elim (m, f) = error "Sin implementar."
+
+-- 5. red. Función que aplica la regla de reducción.
+red :: Solucion -> Solucion
+red (m, f) = error "Sin implementar."
+
+-- 6. split. Función que aplica la regla de la partición de una literal.
+--            Se debe tomar la primer literal que aparezca en la fórmula.
+split :: Solucion -> [Solucion]
+split (m, f) = error "Sin implementar."
+
+-- 7. conflict. Función que determina si la Solucion llegó a una contradicción.
+conflict :: Solucion -> Bool
+conflict (m, f) = error "Sin implementar."
+
+-- 8. success. Función que determina si la fórmula es satisfacible.
+success :: Solucion -> Bool
+success (m, f) = error "Sin implementar."
+
+--9. appDPLL. Función que aplica las reglas anteriores una vez.
+appDPLL :: Solucion -> Solucion
+appDPLL (m, f) = error "Sin implementar."
+
+
+
+{-- Puntos Extra --}
+
+{--
+--dpll. Función que aplica el algoritmo DPLL a una fórmula.
+dpll :: Solucion -> Solucion
+dpll (m, f) = error "Sin implementar."
+--}
